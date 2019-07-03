10455f1c47be932a74af1f3ce03613e17acfb0e5
Luca Schumann <lschumann@omniga.de>
2019-07-03 15:54:40 +0200

    Fixed return view on unsuccessful login

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -76,6 +76,7 @@ namespace WishList.Controllers
             if (!result.Succeeded)
             {
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View("Login", model);
             }
             return RedirectToAction("Index", "Item");
         }
